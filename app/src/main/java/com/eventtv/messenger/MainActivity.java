c9860ebbe03e3b91cb8d57b178a1ac718c259db4
jhbae7395-cmyk <2.34527307e+08+jhbae7395-cmyk@users.noreply.github.com>
2026-07-28 13:02:41 +0000

    fix: R.id.webview ID 오류 수정 (webView → webview)

--- app/src/main/java/com/eventtv/messenger/MainActivity.java
+++ app/src/main/java/com/eventtv/messenger/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = findViewById(R.id.webView);
+        webView = findViewById(R.id.webview);
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
